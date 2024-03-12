7959417e6f5428211903acebc9ee9297142f625b
Dmytro Kagirov <dmytro27kagirov@gmail.com>
2024-03-12 22:20:14 +0100

    IV fixed

--- projects/client/src/app/services/crypto.service.ts
+++ projects/client/src/app/services/crypto.service.ts
@@ -19,6 +19,8 @@ export class CryptoService {
       this.userId = userId;
     });
 
+    console.log('environment.cloudGuardBase64Iv: ', environment.cloudGuardBase64Iv);
+
     const decodedIv = atob(environment.cloudGuardBase64Iv);
     const decodedIvArray = decodedIv.split('').map(char => char.charCodeAt(0));
 
